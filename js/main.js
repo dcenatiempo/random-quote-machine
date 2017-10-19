61c9e2bfcb145cd25a08c5a5fb92ef2d8b1bee90
Devin Cenatiempo <dcenatiempo@gmail.com>
2017-10-18 21:59:05 -0600

    Cleaned up code.

--- js/main.js
+++ js/main.js
@@ -1,39 +1,43 @@
+
 $(document).ready(function(){
   // define the empty quote object
-  let quote =
-  {
+  let quote = {
       quote: "",
       author: "",
       permalink: "",
       tweetURL: "https://twitter.com/intent/tweet?text="
-  }
+  };
   
   // changes html to match new quote
-  let changeQuote = function(quote)
-  {
-      $(".message").html('C:\\> ' + quote.quote);
-      $(".author").html('<br> - ' + quote.author);
-      $(".permalink").html('<br><a class="dos-prompt-text" href="' + quote.permalink + ' "target="_blank">C:\\> Permalink</a>');
+  let changeQuote = function(data) {
+      $(".message").html('C:\\> ' + data.quote);
+      $(".author").html('<br> - ' + data.author);
+      $(".permalink").html('<br><a class="dos-prompt-text" href="' + data.permalink + ' "target="_blank">C:\\> Permalink</a>');
   }
   
   // retreives new JSON quote object from external source
-  // I cant get this to work on CodePen!!!
-  let getRandomQuote = function()
-  {
-        $.getJSON('//quotes.stormconsultancy.co.uk/random.json', function(data)
-          {
-              quote.quote = data.quote;
-              quote.author = data.author;
-              quote.permalink = data.permalink;
-              changeQuote(data);
-          }
-      );
+  let getRandomQuote = function() {
+      fetch('http:quotes.stormconsultancy.co.uk/random.json')
+        .then(function(response) { 
+        // Convert to JSON
+        return response.json();
+      }).then(function(data) {
+          quote.quote = data.quote;
+          quote.author = data.author;
+          quote.permalink = data.permalink;
+          changeQuote(quote);
+      }).catch(function(err) {
+          quote.quote = "oops! something went wrong";
+          quote.author = "error";
+          quote.permalink = "";
+          changeQuote(quote)
+          console.log(err);
+          throw new Error("Failed to fetch!");
+      });
   };
   
   // open up tweet box in new window
-  let tweet = function(url)
-  {
-  //    console.log(quote.quote.split(" ").join("+"));
+  let tweet = function(url) {
       window.open(url.concat(quote.quote.split(" ").join("+")), "_blank");
   };
   
@@ -45,6 +49,4 @@ $(document).ready(function(){
   
   // tweet quote when #tweet clicked
   $("#tweet").click(function(){tweet(quote.tweetURL);});
-  
-  });
-  
+});
